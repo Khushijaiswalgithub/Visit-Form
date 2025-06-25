0311930d9008e3ce041084d22ae00085d7a5c4ea
Khushi Jaiswal <159470469+Khushijaiswalgithub@users.noreply.github.com>
2025-06-25 11:37:30 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 require('dotenv').config(); // Load .env
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000 
 
 // Middleware
 app.use(cors());
